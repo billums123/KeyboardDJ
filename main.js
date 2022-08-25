5b1eeb56955777075b115c7f1639ccdf8bb138e0
billums123 <wthest123@gmail.com>
2022-08-24 23:57:55 -0600

    fixed chrome extension, working prototype

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
-document.addEventListener('DOMContentLoaded', () => {
-
+// console.log('Loaded here');
+// document.addEventListener('DOMContentLoaded', () => {
+    //const audioSampleFilePath = "https://github.com/billums123/KeyboardDJ/blob/main/assets/"
+    // console.log('You made it in!');
     //create backtrack tag
+    const extensionURL = chrome.runtime.getURL(
+        './assets/'
+      );
     let backTrack = document.createElement("audio");
     backTrack.setAttribute("loop", "loop");
     backTrack.setAttribute("autoplay", "autoplay");
@@ -10,22 +15,29 @@ document.addEventListener('DOMContentLoaded', () => {
     let soundEffect = document.createElement("audio")
     soundEffect.setAttribute("autoplay", "autoplay");
     soundEffect.volume = 1;
-
+    
+    let overlayPartyDiv = document.createElement("div")
+    overlayPartyDiv.setAttribute("id", "overlayPartyDiv")
+    document.body.appendChild(overlayPartyDiv)
     //changing background color;
     const setBg = () => {
         //produce a random hexadecimal color between 0 and 255^3 
         const randomColor = Math.floor(Math.random()*16777215).toString(16);
-        document.getElementById("test").style.backgroundColor = "#" + randomColor + "40";
+        overlayPartyDiv.style.backgroundColor = "#" + randomColor + "40";
+        console.log('overlayPartyDiv',overlayPartyDiv)
       }
 
     //DRY method; 
       const updateAudioAndVideo = (audioSrc) => {
-        backTrack.setAttribute("src", `./assets/${audioSrc}.wav`); 
+        backTrack.setAttribute("src", `${extensionURL}${audioSrc}.wav`); 
+        // console.log('audio', `${audioSampleFilePath}${audioSrc}.wav`)
+        console.log('audio', `${extensionURL}${audioSrc}.wav`)
         setBg();
       }
 
       const updateSingleAudio = (audioSrc) => {
-        soundEffect.setAttribute("src", `./assets/${audioSrc}.wav`)
+        
+        soundEffect.setAttribute("src", `${extensionURL}${audioSrc}.wav`)
         setBg();
       }
 
@@ -58,4 +70,4 @@ document.addEventListener('DOMContentLoaded', () => {
     })
 
 
-})
+// })
